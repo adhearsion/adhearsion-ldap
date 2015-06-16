113029da7c00afeed4120c2239db1d25949247c3
Ben Klang <bklang@mojolingo.com>
2015-06-16 12:45:54 -0400

    CS: Ruby 1.9 hash syntax

--- lib/adhearsion/ldap/plugin.rb
+++ lib/adhearsion/ldap/plugin.rb
@@ -13,22 +13,22 @@ module Adhearsion
       # Configure an LDAP connection using ActiveLdap.  See ActiveLdap::Base.establish_connect
       # for further information about the appropriate settings here.
       config :adhearsion_ldap do
-        host                 nil, :desc => "LDAP server host"
-        port                 389, :desc => "LDAP server port"
-        base                  "", :desc => <<-__
+        host                 nil, desc: 'LDAP server host'
+        port                 389, desc: 'LDAP server port'
+        base                  '', desc: <<-__
           LDAP tree that must be used in the connection
         __
-        bind_dn               "", :desc => <<-__
+        bind_dn               '', desc: <<-__
           Distinguished Name to bind as (LDAP auth)
         __
-        password              "", :desc => "Password credentials"
-        allow_anonymous    false, :desc => "valid values: true | false (default)"
-        try_sasl           false, :desc => "valid values: true | false (default)"
-        models "app/ldap_models", :desc => "directory containing ActiveLdap models"
+        password              '', desc: 'Password credentials'
+        allow_anonymous    false, desc: 'valid values: true | false (default)'
+        try_sasl           false, desc: 'valid values: true | false (default)'
+        models 'app/ldap_models', desc: 'directory containing ActiveLdap models'
       end
 
       # Include the LDAP service in plugins initialization process
-      init :adhearsion_ldap do
+      run :adhearsion_ldap do
         Service.start
       end
 
